6505162f700a01591093c0267bd7697eac160d94
aaron-j-aben <aaron.jm.aben@gmail.com>
2024-11-07 16:54:15 -0500

    Initial attempt to implement removeFromArray function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,22 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    for (const arg of args) {
+        for (let i = 0; i < arr.length; i++) {
+            if (arr.at(i) === arg) {
+                arr.splice(i, 1);
+            }
+        }
+    }
+
+    return arr;
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
+/*
+Things to learn:
+ >> indefinite optional arguments
+ >> Common array methods - splice
+ - How array length is calculated/stored in js
+*/
