7e6b31d40ae96b3858e0c241f82d9753c73f443d
aaron-j-aben <aaron.jm.aben@gmail.com>
2024-11-07 16:31:58 -0500

    Implement reverseString function
    Make array from string since string is immutable. Swap per index until
    the halfway point. Use join to restitch into a string.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    const strArr = string.split('');
+    const halfLen = Math.floor(string.length / 2);
+    
+    for (let i = 0; i < halfLen; i++) {
+        [strArr[i], strArr.at(-i - 1)] = [strArr.at(-i - 1), [strArr[i]]]
+    }
 
+    return strArr.join('');
 };
 
 // Do not edit below this line
