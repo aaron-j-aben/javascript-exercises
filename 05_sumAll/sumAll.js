5eecb5ef9cd279109df76db9643eb01cc8c466d1
aaron-j-aben <aaron.jm.aben@gmail.com>
2024-11-07 17:09:44 -0500

    Initial attempt to implement sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(n, m) {
+    if (!(Number.isInteger(n) && Number.isInteger(m))) {
+        return 'ERROR';
+    }
 
+    let start, end, sum = 0;
+    if (n < m) {
+        start = n;
+        end = m;
+    } else {
+        start = m;
+        end = n;
+    }
+
+    while (start <= end) {
+        sum += start++;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
