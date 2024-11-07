74425922036c9f6101ff5bf48f6e4d567411e0f4
aaron-j-aben <aaron.jm.aben@gmail.com>
2024-11-07 17:31:35 -0500

    Implement convertToCelsius function

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,10 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  const tempInCelsius = (tempF - 32) * (5/9);
+
+  return tempInCelsius.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
 };
 
 // Do not edit below this line
