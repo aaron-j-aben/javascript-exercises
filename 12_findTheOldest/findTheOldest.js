728b89554ea08ed20c1a06addbb02d019c5e07d5
aaron-j-aben <aaron.jm.aben@gmail.com>
2024-11-11 13:45:35 -0500

    Implement and test findTheOldest function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const currentYear = new Date().getFullYear();
+    let oldestAge = 0, oldestPerson = null;
+    for (let person of people) {
+        let age = 0;
+        if ('yearOfDeath' in person) {
+            age = person.yearOfDeath - person.yearOfBirth;
+        } else {
+            age = currentYear - person.yearOfBirth;
+        }
 
+        if (age > oldestAge) {
+            oldestAge = age;
+            oldestPerson = person;
+        };
+    }
+
+    return oldestPerson;
 };
 
 // Do not edit below this line
