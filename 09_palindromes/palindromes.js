eaee7928c00bb7f66e31002901756861c9374010
aaron-j-aben <aaron.jm.aben@gmail.com>
2024-11-11 13:20:17 -0500

    Implement and test palindromes function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const charArr = str.toLowerCase().split('');
+    const cleanedArr = charArr.filter((char) => {
+        const alphaNumericRegex = /^[0-9a-zA-Z]+$/;
+        return char !== ' ' && char.match(alphaNumericRegex);
+    });
+    
+    let start = 0, end = cleanedArr.length - 1;
 
+    while (start < end) {
+        if (cleanedArr[start] !== cleanedArr[end]) return false;
+        start++, end--;
+    }
+
+    return true;
 };
 
 // Do not edit below this line
