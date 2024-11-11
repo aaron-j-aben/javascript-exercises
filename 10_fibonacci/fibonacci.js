9a56db22dbcb51e6376e27ae1dcabcb5f099a186
aaron-j-aben <aaron.jm.aben@gmail.com>
2024-11-11 13:25:51 -0500

    Implement and test fibonacci function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (n < 0) return 'OOPS';
 
+    let temp, pre = 0, curr = 1;
+
+    for (let i = 0; i < n; i++) {
+        temp = pre;
+        pre = curr;
+        curr = pre + temp;
+    }
+
+    return pre;
 };
 
 // Do not edit below this line
